5c41557c6b76208d33801ad0a6f327d296462aa2
Gus Antoniassi <gus.antoniassi@gmail.com>
2017-12-04 19:04:33 -0200

    Fill em andamento - falta fazer preencher a tela de preto corretamente

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,87 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// var lastpress = 0
+// while (true) {
+//     var currpress = read(KBD)
+//     var whichfill = 0;
+//     if (currpress > 0) {
+//         whichfill = -1;
+//     }
+//     // preencher a tela
+//     for (i = 0; i < @KBD; i++) {
+//        screen[i] = whichfill;
+//     }
+// }
+
+    @lastpress
+    M=0
+
+    (WHILETRUE)
+        // Lê o valor do KBD e armazena na variável currpress
+        @KBD
+        D=M
+        @currpress
+        M=D
+
+        @SETFILLBLACK
+        M;JGT
+
+        // else
+        @FILLSCREENLOOP
+        0;JMP
+
+    // Seta a variável @whichfill como -1
+    (SETFILLBLACK)
+        @whichfill
+        M=-1
+        @FILLSCREENLOOP
+        0;JMP
+
+    // for (i = 0; i < @KBD; i++)
+    (FILLSCREENLOOP)
+        // set i = 0
+        @i
+        M=0
+        D=M
+        (LOOP)
+            @i
+            D=M
+            // if (i < @KBD)
+            @KBD
+            D=D-A
+            // encerrar o loop (voltar pro while) se i - @KBD < 0
+            @WHILETRUE
+            D;JLT
+
+            // if (whichfill == 0) { JMP @FILLSCREENWHITE }
+                @whichfill
+                D=M
+                @FILLSCREENWHITE
+                D;JEQ
+            // else            
+                // Preenche a tela com a cor preta
+                @i
+                D=M
+                @SCREEN
+                A=D+A
+                M=-1
+                @LOOPEND
+                0;JMP
+
+            // Preenche a tela com a cor branca
+            (FILLSCREENWHITE)
+                @i
+                D=M
+                @SCREEN
+                A=D+A
+                M=0
+                @LOOPEND
+                0;JMP
+            
+           (LOOPEND)
+           @i
+           M=M+1
+           @LOOP
+           0;JMP
